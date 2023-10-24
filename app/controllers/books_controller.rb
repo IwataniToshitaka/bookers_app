0280428b816b132df909dfb8644fc6c14ba5604f
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-10-24 11:14:42 +0000

    [Continue]3-5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,42 @@
 class BooksController < ApplicationController
   def new
+      #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+      @book = Book.new
+  end
+
+  def create
+
+
+    #データを受け取り新規登録のインスタンス作成
+    #paramsにはformから送られてくるデータが入っている
+    book = Book.new(list_params)
+    #データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    #topへリダイレクト
+    redirect_to '/top'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @books = Book.all
   end
 
   def edit
+    @books = Book.all
+  end
+
+  def destroy
+    book = Book.find(params[:id]) #データ(レコード)を１件取得
+    book.destroy #データ(レコード)を削除
+    redirect_to '/books'  #投稿一覧画面へリダイレクト
+  end
+  #これより下はcontroller内のみ呼び出す
+  private
+  #ストロングデータ
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
