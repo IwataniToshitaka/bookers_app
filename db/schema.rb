7b87cd6c2729ceef4d23e6e922726f0169c1961b
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-11-02 09:36:28 +0000

    [Finish]bookers

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2023_10_23_152959) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
